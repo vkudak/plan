714d7a1da03fb9b52e491c46071a5ee02d1ce433
vkudak <vkudak@ukr.net>
2026-01-08 22:39:37 +0200

    Fix deprecated tle_latest in spacetrack

--- get_tle_st.py
+++ get_tle_st.py
@@ -40,7 +40,8 @@ st = SpaceTrackClient(username, password)
 
 ndate = datetime.datetime.now().strftime("%Y%m%d")
 list_my = read_planed_objects(objects_file)
-data = st.tle_latest(norad_cat_id=list_my, ordinal=1, epoch='>now-30', format='3le')
+# data = st.tle_latest(norad_cat_id=list_my, ordinal=1, epoch='>now-30', format='3le')
+data = st.gp(norad_cat_id=list_my, epoch='>now-30', format='3le')
 
 if os.path.isdir('tle'):
     # fr = open("tle//tle_ckkp_" + ndate + ".txt", "w")
